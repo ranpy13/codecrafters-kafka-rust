a6c0363144b508d43a9e5d56c2f2384cb55f818c
ranpy13 <85052085+ranpy13@users.noreply.github.com>
2025-08-15 14:21:00 +0530

    fix(response): stream message size

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
+
+use crate::model::WireProtocol;
+
+mod model;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,6 +15,9 @@ fn main() {
         match stream {
             Ok(_stream) => {
                 println!("accepted new connection");
+                // return WireProtocol::Response::new(8, "v0", "some random body");
+                let mut _stream = _stream;
+                _stream.write_all(&[0, 0, 0, 0, 0, 0, 0, 0]).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
---
+++ src/model/WireProtocol.rs
@@ -0,0 +1,29 @@
+pub struct Request {
+    // to figure out
+}
+
+pub struct Response {
+    pub message_size: i32,
+    pub header: String,
+    pub body: String,
+}
+
+impl Response {
+    pub fn new(message_size: i32, header: String, body: String) {
+        assert!(message_size.is_positive());
+        assert!(!header.is_empty());
+        assert!(!body.is_empty());
+
+        Self {
+            message_size: message_size,
+            header: header,
+            body: body,
+        }
+    }
+}
+
+impl Request {
+    pub fn new() {
+        // need to figure out...
+    }
+}
